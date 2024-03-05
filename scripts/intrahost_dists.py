1e11c90cb6e51f0ffa4b72fe7e455cec81aab52d
Niema Moshiri <niemamoshiri@gmail.com>
2024-03-04 16:05:42 -0800

    Updated to add tn93 args

--- scripts/intrahost_dists.py
+++ scripts/intrahost_dists.py
@@ -9,10 +9,19 @@ from subprocess import check_output, DEVNULL, PIPE
 from sys import argv
 import argparse
 
+# constants
+TN93_BASE_COMMAND = [
+    'tn93',
+    '-l', '1', # at least 1 overlap
+]
+
 # parse user args
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input', required=False, type=str, default='stdin', help="Input FASTA")
+    parser.add_argument('-t', '--threshold', required=False, type=float, default=1., help="TN93 Distance Threshold")
+    parser.add_argument('-g', '--ambig_fraction', required=False, type=float, default=1., help="TN93 Ambiguity Fraction")
+    parser.add_argument('-o', '--output', required=False, type=str, default='stdout', help="Output File")
     args = parser.parse_args()
     return args
 
@@ -55,21 +64,36 @@ def load_seqs_per_person(seqs):
 # calculate all pairwise distances between sequences from the same individual (this is the main logic of this script)
 # currently not parallelizing across people because tn93 parallelizes pairwise distance calculations,
 # but we could easily parallelize across people as well
-def calc_intrahost_dists(seqs, seqs_per_person):
+def calc_intrahost_dists(seqs, seqs_per_person, out_fn, tn93_t, tn93_g):
+    # open output file
+    if out_fn.strip().lower() == 'stdout':
+        from sys import stdout as output
+    elif out_fn.strip().lower().endswith('.gz'):
+        output = gopen(out_fn, 'wt')
+    else:
+        output = open(out_fn, 'w')
+
+    # build tn93 command
+    tn93_command = TN93_BASE_COMMAND # start with base command
+    tn93_command += ['-t', tn93_t]   # add max distance threshold
+    tn93_command += ['-g', tn93_g]   # add ambiguity fraction
+
+    # run on all people
     for person_num, person in enumerate(seqs_per_person):
         if len(seqs_per_person[person]) < 2:
             continue # skip people with just 1 sequence
         curr_fasta = '\n'.join('%s\n%s' % (k,seqs[k]) for k in seqs_per_person[person])
-        o = check_output(['tn93', '-t', '0.03', '-l', '1'], stderr=DEVNULL, input=curr_fasta, encoding='ascii')
+        o = check_output(tn93_command, stderr=DEVNULL, input=curr_fasta, encoding='ascii')
         if person_num == 0:
-            print(o.strip())
+            output.write(o)
         else:
             for l in o.splitlines()[1:]:
-                print(l.strip())
+                output.write(l)
+    output.close()
 
 # run tool
 if __name__ == "__main__":
     args = parse_args()
     seqs = load_fasta(args.input)
     seqs_per_person = load_seqs_per_person(seqs)
-    calc_intrahost_dists(seqs, seqs_per_person)
+    calc_intrahost_dists(seqs, seqs_per_person, args.threshold, args.ambig_fraction)
