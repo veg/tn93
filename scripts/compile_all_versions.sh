9dd781525d9df593b1aaa356426b12fd01e7ad83
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-02 11:27:18 -0800

    Added ability to pass in make arguments

--- scripts/compile_all_versions.sh
+++ scripts/compile_all_versions.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # compile all versions of tn93 into the current working directory
+# USAGE: ./compile_all_versions [MAKE_FLAGS]
 wget -qO- "https://api.github.com/repos/veg/tn93/releases" | grep '"tarball_url"' | cut -d'"' -f4 | while read url ; do
     version=$(echo "$url" | rev | cut -d'/' -f1 | rev)
     wget -qO- "$url" | tar -zx
     cd veg-tn93-*
     cmake .
-    make
+    make $@
     mv tn93 "../tn93.$version"
     cd ..
     rm -rf veg-tn93-*
+    exit # TODO DELETE
 done
