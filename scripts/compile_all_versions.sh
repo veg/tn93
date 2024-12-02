b2851609e7a0e30261eca7bf0d5cf1c46aa4cab8
Niema Moshiri <niemamoshiri@gmail.com>
2024-12-02 11:25:00 -0800

    Updated comment

--- scripts/compile_all_versions.sh
+++ scripts/compile_all_versions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# compile all versions of tn93 into a given directory
+# compile all versions of tn93 into the current working directory
 wget -qO- "https://api.github.com/repos/veg/tn93/releases" | grep '"tarball_url"' | cut -d'"' -f4 | while read url ; do
     version=$(echo "$url" | rev | cut -d'/' -f1 | rev)
     wget -qO- "$url" | tar -zx
